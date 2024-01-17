fab9033aed65bb86b120e3022c496be737d355de
jerrykankelborg <jkankelborg@csumb.edu>
2024-01-17 14:40:39 -0800

    Make the nav work and optimize images

--- js/script.js
+++ js/script.js
@@ -3,7 +3,11 @@
 
 const button = document.querySelector(".btn-mobile-nav");
 const header = document.querySelector(".header");
+const openIcon = document.querySelector(".open-icon");
+const closeIcon = document.querySelector(".close-icon");
 
 button.addEventListener("click", () => {
   header.classList.toggle("nav-open");
 });
+
+//////////////////////////////////////////////////////
